95a688ac9f5e297ae8de468256327a30b90e90f3
Miniii-git <125822323+Miniii-git@users.noreply.github.com>
2023-10-29 07:39:05 +0330

    make search enginr

--- src/Search.js
+++ src/Search.js
@@ -1,30 +1,56 @@
 import React, { useState } from "react";
 import "./Search.css";
+import axios from "axios";
 
 export default function Search() {
   let [city, setCity] = useState("");
   let [weather, setWeather] = useState(null);
   let apiKey = "t734d4903fba534f1644oba02ab79462";
 
+  function showWeather(response) {
+    setWeather(response.data);
+  }
+
   function hndleSubmit(event) {
     event.preventDefault();
     let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}`;
+    axios.get(apiUrl).then(showWeather);
+    console.log(weather);
   }
 
   function updateCityName(event) {
     event.preventDefault();
     setCity(event.target.value);
   }
-  return (
-    <div className="Search">
-      <form onSubmit={hndleSubmit}>
-        <input
-          type="sesrch"
-          placeholder="Enter a City ..."
-          onChange={updateCityName}
-        />
-        <input type="submit" value="Search" />
-      </form>
-    </div>
+
+  let form = (
+    <form onSubmit={hndleSubmit}>
+      <input
+        type="sesrch"
+        placeholder="Enter a City ..."
+        onChange={updateCityName}
+      />
+      <input type="submit" value="Search" />
+    </form>
   );
+
+  if (weather) {
+    return (
+      <div className="Search">
+        {form}
+        <div className="weather-information">
+          <h2>{weather.city}</h2>
+          <h4>{weather.condition.description}</h4>
+          <h4>Temperature: {Math.round(weather.temperature.current)}°C</h4>
+          <h4>humidity: {Math.round(weather.temperature.current)}%</h4>
+          <img
+            src={weather.condition.icon_url}
+            alt={weather.condition.description}
+          />
+        </div>
+      </div>
+    );
+  } else {
+    return <div className="Search">{form}</div>;
+  }
 }
